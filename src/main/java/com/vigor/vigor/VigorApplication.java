58dfe4c39e3858783ed363af34390ed1060db513
moracicero <135073675+moracicero@users.noreply.github.com>
2026-02-23 16:25:03 -0300

    Init Spring Boot project structure

---
+++ src/main/java/com/vigor/vigor/VigorApplication.java
@@ -0,0 +1,13 @@
+package com.vigor.vigor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigorApplication.class, args);
+	}
+
+}
