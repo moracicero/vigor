bed55ca71f14f016f506f80469c933bbca47e6d7
moracicero <135073675+moracicero@users.noreply.github.com>
2026-02-24 19:39:47 -0300

    base web

---
+++ src/main/java/com/vigor/vigor/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.vigor.vigor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/servicios")
+    public String servicios() {
+        return "servicios"; 
+    }
+    @GetMapping("/tienda")
+    public String tienda() {
+        return "tienda"; 
+    }
+
+    @GetMapping("/contacto")
+    public String contacto() {
+        return "contacto"; 
+    }
+}
